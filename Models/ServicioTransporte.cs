ff928af3c7f066b6c55ea39e2b19beb6810b8b9d
Alejandro Parra <xjuanchito102371x@gmail.com>
2023-11-19 15:32:40 -0500

    Agregar archivos de proyecto.

---
+++ Models/ServicioTransporte.cs
@@ -0,0 +1,15 @@
+﻿namespace TrasladoSeguro.Models
+{
+    public class ServicioTransporte
+    {
+        public int Id {  get; set; }
+
+        public string? NombreConductor { get; set; }
+
+        public string? Description {  get; set; }
+
+        public ICollection<Cliente>? Clientes { get; set; } = default!;
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TrasladoSeguro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
